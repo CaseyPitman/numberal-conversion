8b6d321e42a1afe33bfd807b299f3e22c5781579
Casey The Great <casey@caseypitman.com>
2020-08-03 12:29:31 -0500

    HTML set. App.js structure outlined.

---
+++ app.js
@@ -0,0 +1,78 @@
+//APP TO CONVERT BETWEEN ROMAN AND STANDARD NUMERALS
+
+// ************ DATA ***********
+
+const data = () =>{
+
+   //Determine type of conversion by reading input
+      //if input contains letters then return RtoS
+      //else if input contains numerals then return RtoS
+      //else, you screwed up. Try again. 
+      //return type
+   
+   //convert toStandard 
+      //return standard
+   
+   //convert toRoman
+      //return Roman
+   
+};
+
+// ************ UI ***********
+
+const ui = () => {
+   
+ 
+   //Get input
+      //return input
+   
+   //Display results
+
+      //Change inner text for result
+      //Display none main page
+      //Display block result
+
+      //return
+
+
+   //Clear results
+      //return
+};
+
+// ************ CONTROLLER ***********
+
+const controller = ((data, ui) => {
+   //Get input - UI
+
+   //Determine type of conversion -DATA 
+      //pass input
+
+   //Make conversion - pass input and type
+      //If R to S call toStandard
+         //result =
+      //Else if S to R call toRoman
+         //result =
+
+   //Display results - pass input and result
+
+
+return {
+   init: () =>{
+      console.log('Initialization');
+      //clear page and set up page
+         //set selector to "selected"
+         //Make select visible
+         //Hide result
+   }
+}
+
+})(data,ui);
+
+controller.init();
+
+//Event listeners
+   //Convert button click
+
+   //Convert button enter
+
+   //Reset page click (init);
