6169e462fa627113a28f093982e340d14bc235ee
Casey The Great <casey@caseypitman.com>
2020-08-04 17:22:35 -0500

    Core conversion functions operational. Need to test edge cases.

--- app.js
+++ app.js
@@ -30,17 +30,21 @@ const dataCtrl = (() =>{
             //No input given
             alert(`You have to, you know, enter a number here bub. It can be Roman or Standard, but it's gotta be something. Come on. Get it together.`);
             controller.init();
+            
          } else if (stanRegEx.test(input) && input < 4000 && input > 0){  
             //Correctly formatted number of the proper amount.
             type = 'StanToRom';
+            
          } else if (stanRegEx.test(input) && input >= 4000) { 
             //Too high a number
             alert ('Yeah, so that number is too high. Roman numerals are limited to numbers below 4000. Can you imagine a world where the highest number was 3999?');
             controller.init();
+            
          } else if (stanRegEx.test(input) && input < 1){ 
             //Too low number
             alert(`Okay, so the Romans did't have a concept of zero, and certinaly no concept of negative numbers. Wild, eh? Does this mean there was no debt...?`)
             controller.init();
+            
          } else if (romanRegEx.test(input)){ 
             //Correctly formatted Roman numeral
             type = 'RomToStan';
@@ -48,6 +52,7 @@ const dataCtrl = (() =>{
             //Letters but not a proper Roman numeral
             alert(`You're killing me here, bub. I'm not sure what you entered, but it wasn't a Roman numeral and it wasn't a Standard numeral. You typed your name didn't you? Or maybe some naughty words. Grow up and try again.`);
             controller.init();
+           
          }
          else { 
             //Just plain screwed it up. Most likely a combination of letters and numbers.
@@ -60,46 +65,134 @@ const dataCtrl = (() =>{
       //Convert from Standard to Roman
       toRoman : (input) => {
 
-         //TESTING
-         // input = '1994';
          let length = input.length;
          let result = "";
-         let placeValArr = [];
+         let valArr = [];
 
          let multiplier = 1;
          for (let i = length-1; i >= 0 ; i--){
-            placeValArr.unshift(input[i] * multiplier);
+            valArr.unshift(input[i] * multiplier);
             multiplier *= 10;
          }
-         console.log(placeValArr);
 
-         //Thousands 
-         if (length === 4){
-            let thousands = placeValArr[0]/1000;
+         //THOUSANDS
+         if (valArr[0] >= 1000){
+            let thousands = valArr[0]/1000;
             for (let i = 0; i < thousands; i++){
-               result += "M";
+               result += 'M';
             }
+            valArr.shift();
+         }
+         
+         //HUNDREDS
+         //Zero in hundreds place
+         if (valArr[0]===0){
+            valArr.shift();
+         }
+         //900
+         else if (valArr[0] === 900) {  
+            result += 'CM';
+            valArr.shift();
+         } 
+         //500
+         else if(valArr[0] === 500){ //Exactly 500
+            result += 'D';
+            valArr.shift();
+         }
+         else if (valArr[0] > 500){ //More than 500. Some hundreds will be left
+            result += 'D';
+            valArr[0] = valArr[0] - 500;
+         }
+         //400
+         else if (valArr[0] === 400){
+            result += 'CD';
+            valArr.shift();
+         }
+         //100 - 300
+         if (valArr[0] >= 100){
+            let hundreds = valArr[0]/100;
+            for (let i = 0; i < hundreds; i++){
+               result += 'C';
+            }
+            valArr.shift();
          }
 
-         //Hundreds
+         //TENS
 
+         //Zero in tens place
+         if (valArr[0]===0){
+            valArr.shift();
+         }
+         //90
+         else if (valArr[0] === 90){
+            result += 'XC';
+            valArr.shift();
+         }
+         //50
+         else if (valArr[0] ===  50){
+            result += 'L';
+            valArr.shift();
+         } 
+         //Between 90 and 50 - some tens leftover;
+         else if (valArr[0] > 50){
+            result += 'L';
+            valArr[0] = valArr[0] - 50;
+         }
+         //40
+         else if (valArr[0] === 40){
+            result += 'XL';
+            valArr.shift();
+         }
+         //Tens between 10 and 40 (including those leftover from removing 50
+         if (valArr[0] >= 10) {
+            let tens = valArr[0] / 10;
+            for (let i = 0; i < tens; i++){
+               result += 'X';
+            }
+            valArr.shift();
+         }
 
+         //ONES
+         //Zero in ones place
+         if (valArr[0]===0){
+            valArr.shift();
+         }
+         //Nine
+         else if (valArr[0] === 9){
+            result += 'IX';
+            valArr.shift();
+         }
+         //5
+         else if (valArr[0] ===  50){
+            result += 'V';
+            valArr.shift();
+         } 
+         //Between 9 and 5 - some ones leftover;
+         else if (valArr[0] > 5){
+            result += 'V';
+            valArr[0] = valArr[0] - 5;
+         }
+         //4
+         else if (valArr[0] === 4){
+            result += 'IV';
+            valArr.shift();
+         }
 
-         //Tens
-
-
-
-         //ones
-
+         //Ones between 1 and 4 (including those leftover from removing 5)
+         if (valArr[0] >= 1) {
+            let ones = valArr[0];
+            for (let i = 0; i < ones; i++){
+               result += 'I';
+            }
+            valArr.shift();
+         }
          return result;
-
       },
 
       // Convert from Roman to Standard
       toStandard : (input) => {
          //make all the letters upper case, just in case.
          let num = input.toUpperCase();
-         console.log(num);
          let romanArray = [];
          let integerArray = [];
          const romanValues = {
@@ -134,13 +227,6 @@ const dataCtrl = (() =>{
          return result;
       }
    }
-
-   //convert toStandard 
-   //return standard
-   
-   //convert toRoman
-   //return Roman
-   
 })();
 
 // ************ UI ***********
@@ -203,7 +289,6 @@ const controller = ((data, ui) => {
       
       //Get input from UI. 
       input = ui.getInput();
-      console.log(`input: ${input}`);
 
       //Check input for commas and remove them
       if (input.includes(',')){
@@ -212,9 +297,7 @@ const controller = ((data, ui) => {
 
       //Check input for type
       type = data.getType(input);
-      console.log(`type: ${type}`);
-
-
+      //Make conversion dependent on type determined.
       if (type === `StanToRom`){
          //Convert a standard number to a roman number. 
          result = data.toRoman(input);
@@ -228,15 +311,11 @@ const controller = ((data, ui) => {
       
 };
       
-
-   
    return {
       init: () =>{
          console.log('Initialization');
-         //clear page and set up page
-         //set selector to "selected"
-         //Make select visible
-         //Hide result
+         //ui.clear
+
          eventListeners();
       }
    }
@@ -244,11 +323,3 @@ const controller = ((data, ui) => {
 })(dataCtrl, uiCtrl);
 
  controller.init();
-
-
-//Event listeners
-   //Convert button click
-   // document.getElementById("convert-btn").addEventListener('click', controller)
-   //Convert button enter
-
-   //Reset page click (init);
