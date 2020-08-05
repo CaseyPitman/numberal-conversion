41d217d86b524940737eaece943f5e3c2738a584
Casey The Great <casey@caseypitman.com>
2020-08-04 19:32:01 -0500

    refactored standard to roman function

--- app.js
+++ app.js
@@ -5,14 +5,74 @@
 //LIMIT TO LESS THAN 4000!
 
 const dataCtrl = (() =>{
-   //Data objects will be needed here.
 
-   //This may be better
+   //HELPER FUNCTIONS FOR CONVERTING STANDARD NUMBERS TO ROMAN
 
-   const romanArr = ['I', 'V', 'X', 'L', 'C', 'D', 'M'];
-   const standArr = [1, 5, 10, 50, 100, 500, 1000];
+   //Determing Roman for thousands place.
+   const romanThousands = (num) =>{
+      const thousands = {
+         0 : '',
+         1 : 'M',
+         2 : 'MM',
+         3 : 'MMM'
+      };
+      return thousands[num];
+   };
+
+   //Determing Roman for hundreds place.
+   const romanHundreds = (num) =>{
+      const hundreds = {
+         0 : '',
+         1 : 'C',
+         2 : 'CC',
+         3 : 'CCC',
+         4 : 'CD',
+         5 : 'D',
+         6 : 'DC',
+         7 : 'DCC',
+         8 : 'DCCC',
+         9 : 'CM'
+      };
+      return hundreds[num];
+   };
+
+   //Determing Roman for tens place.
+   const romanTens = (num) =>{
+      const tens = {
+         0 : '',
+         1 : 'X',
+         2 : 'XX',
+         3 : 'XXX',
+         4 : 'LX',
+         5 : 'L',
+         6 : 'LX',
+         7 : 'LXX',
+         8 : 'LXXX',
+         9 : 'CX'
+      }
+      return tens[num];
+   };
+
+   //Determing Roman for ones place.
+   const romanOnes = (num) =>{
+      const ones = {
+         0 : '',
+         1 : 'I',
+         2 : 'II',
+         3 : 'III',
+         4 : 'IV',
+         5 : 'V',
+         6 : 'VI',
+         7 : 'VII',
+         8 : 'VIII',
+         9 : 'XI'
+      };
+      return ones[num];
+   };
+
 
    return {
+      //Formats number input to remove commas
       removeCommas : (input) => {
          input = input.replace(',', "");
          return input;
@@ -21,11 +81,15 @@ const dataCtrl = (() =>{
       //Determine type of conversion by reading input
       getType : (input) => {
          let type, romanRegEx, stanRegEx, letterRegEx;
+         //REGEX
+         //Validate Roman numeral format
          romanRegEx = /^(?=[MDCLXVI])M*(C[MD]|D?C{0,3})(X[CL]|L?X{0,3})(I[XV]|V?I{0,3})$/ig;
+         //Validate standard number
          stanRegEx = /[0-9]/g;
+         //Check for Roman numeral input that may not include all letters. 
          letterRegEx = /[a-z]/gi;
 
-         //Check input
+         //Check input type
          if (!input){  
             //No input given
             alert(`You have to, you know, enter a number here bub. It can be Roman or Standard, but it's gotta be something. Come on. Get it together.`);
@@ -39,7 +103,7 @@ const dataCtrl = (() =>{
             //Too high a number
             alert ('Yeah, so that number is too high. Roman numerals are limited to numbers below 4000. Can you imagine a world where the highest number was 3999?');
             controller.init();
-            
+
          } else if (stanRegEx.test(input) && input < 1){ 
             //Too low number
             alert(`Okay, so the Romans did't have a concept of zero, and certinaly no concept of negative numbers. Wild, eh? Does this mean there was no debt...?`)
@@ -48,13 +112,13 @@ const dataCtrl = (() =>{
          } else if (romanRegEx.test(input)){ 
             //Correctly formatted Roman numeral
             type = 'RomToStan';
+
          } else if(!romanRegEx.test(input) && letterRegEx.test(input)){ 
             //Letters but not a proper Roman numeral
             alert(`You're killing me here, bub. I'm not sure what you entered, but it wasn't a Roman numeral and it wasn't a Standard numeral. You typed your name didn't you? Or maybe some naughty words. Grow up and try again.`);
             controller.init();
-           
-         }
-         else { 
+
+         } else { 
             //Just plain screwed it up. Most likely a combination of letters and numbers.
             alert (`Okay, I'm not saying you are dumb or anything, but let's get it together. ONLY STANDARD NUMERALS OR ONLY ROMAN NUMERALS. Pick one for Pete's sake. And let's keep it to POSITIVE numbers less than 4000. Roman numerals can't go any higher. Okay...let's try this one more time.`)
             controller.init();
@@ -64,127 +128,47 @@ const dataCtrl = (() =>{
 
       //Convert from Standard to Roman
       toRoman : (input) => {
-
+         
          let length = input.length;
          let result = "";
-         let valArr = [];
+         let values = [];
 
          let multiplier = 1;
+         //Array of place values of each digit in the input number. 
          for (let i = length-1; i >= 0 ; i--){
-            valArr.unshift(input[i] * multiplier);
+            values.unshift(input[i] * multiplier);
             multiplier *= 10;
          }
+         //Convert each place value.
 
-         //THOUSANDS
-         if (valArr[0] >= 1000){
-            let thousands = valArr[0]/1000;
-            for (let i = 0; i < thousands; i++){
-               result += 'M';
-            }
-            valArr.shift();
-         }
-         
-         //HUNDREDS
-         //Zero in hundreds place
-         if (valArr[0]===0){
-            valArr.shift();
-         }
-         //900
-         else if (valArr[0] === 900) {  
-            result += 'CM';
-            valArr.shift();
-         } 
-         //500
-         else if(valArr[0] === 500){ //Exactly 500
-            result += 'D';
-            valArr.shift();
-         }
-         else if (valArr[0] > 500){ //More than 500. Some hundreds will be left
-            result += 'D';
-            valArr[0] = valArr[0] - 500;
-         }
-         //400
-         else if (valArr[0] === 400){
-            result += 'CD';
-            valArr.shift();
-         }
-         //100 - 300
-         if (valArr[0] >= 100){
-            let hundreds = valArr[0]/100;
-            for (let i = 0; i < hundreds; i++){
-               result += 'C';
-            }
-            valArr.shift();
+         //Thousands place
+         if (values[0] >= 1000){
+            result += romanThousands(values[0]/1000);
+            values.shift();
          }
 
-         //TENS
-
-         //Zero in tens place
-         if (valArr[0]===0){
-            valArr.shift();
-         }
-         //90
-         else if (valArr[0] === 90){
-            result += 'XC';
-            valArr.shift();
-         }
-         //50
-         else if (valArr[0] ===  50){
-            result += 'L';
-            valArr.shift();
-         } 
-         //Between 90 and 50 - some tens leftover;
-         else if (valArr[0] > 50){
-            result += 'L';
-            valArr[0] = valArr[0] - 50;
-         }
-         //40
-         else if (valArr[0] === 40){
-            result += 'XL';
-            valArr.shift();
-         }
-         //Tens between 10 and 40 (including those leftover from removing 50
-         if (valArr[0] >= 10) {
-            let tens = valArr[0] / 10;
-            for (let i = 0; i < tens; i++){
-               result += 'X';
-            }
-            valArr.shift();
+          //Hundreds place
+          if (values[0] >= 100){
+            result += romanHundreds(values[0]/100);
+            values.shift();
+         } else if (values[0] === 0){
+            values.shift();
          }
 
-         //ONES
-         //Zero in ones place
-         if (valArr[0]===0){
-            valArr.shift();
-         }
-         //Nine
-         else if (valArr[0] === 9){
-            result += 'IX';
-            valArr.shift();
-         }
-         //5
-         else if (valArr[0] ===  50){
-            result += 'V';
-            valArr.shift();
-         } 
-         //Between 9 and 5 - some ones leftover;
-         else if (valArr[0] > 5){
-            result += 'V';
-            valArr[0] = valArr[0] - 5;
-         }
-         //4
-         else if (valArr[0] === 4){
-            result += 'IV';
-            valArr.shift();
+          //Tens place
+          if (values[0] >= 10){
+            result += romanTens(values[0]/10);
+            values.shift();
+         } else if (values[0] === 0){
+            values.shift();
          }
 
-         //Ones between 1 and 4 (including those leftover from removing 5)
-         if (valArr[0] >= 1) {
-            let ones = valArr[0];
-            for (let i = 0; i < ones; i++){
-               result += 'I';
-            }
-            valArr.shift();
+          //Ones place
+          if (values[0] >= 1){
+            result += romanOnes(values[0]);
+            values.shift();
+         } else if (values[0] === 0){
+            values.shift();
          }
          return result;
       },
@@ -312,7 +296,7 @@ const controller = ((data, ui) => {
 };
       
    return {
-      init: () =>{
+      init: () => {
          console.log('Initialization');
          //ui.clear
 
